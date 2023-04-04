3b3d9777762c4b259d2478aa2e4844dc04a11c86
Krei <22022887@slu.edu.ph>
2023-04-05 06:10:48 +0800

    basic functionality for testing

---
+++ generate_random_image.py
@@ -0,0 +1,9 @@
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained("./ds-2-1-base")
+pipe = pipe.to("cuda")
+
+prompt = "abstract realism art fusion"
+image = pipe(prompt).images[0]
+
+image.save(f"random_image.png")
---
+++ get_pretrained_model.py
@@ -0,0 +1,7 @@
+from diffusers import StableDiffusionPipeline
+
+print("fetching pretrained model...")
+pipeline = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1-base")
+
+pipeline.save_pretrained("sd-2-1-base")
+print("saved on sd-2-1-base")
